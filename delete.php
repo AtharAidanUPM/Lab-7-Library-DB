684318c0888c7eeb046ada332e0de4f68ac63979
AtharAidanUPM <218319@student.upm.edu.my>
2025-05-22 01:45:38 +0800

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,9 @@
+<?php
+include 'config.php';
+$id = $_GET['id'];
+if ($conn->query("DELETE FROM library WHERE id=$id")) {
+    echo "Book deleted. <a href='index.php'>Back to list</a>";
+} else {
+    echo " Error: " . $conn->error;
+}
+?>
